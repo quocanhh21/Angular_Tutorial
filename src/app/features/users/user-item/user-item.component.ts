c585da16bc5aad33bbace0883c9ca86cd3ca418b
anh.le <anh.le@s3corp.com.vn>
2025-03-14 16:44:07 +0700

    Init structure file

---
+++ src/app/features/users/user-item/user-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  imports: [],
+  templateUrl: './user-item.component.html',
+  styleUrl: './user-item.component.scss'
+})
+export class UserItemComponent {
+
+}
---
+++ src/app/shared/components/my-table/my-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-table',
+  imports: [],
+  templateUrl: './my-table.component.html',
+  styleUrl: './my-table.component.scss'
+})
+export class MyTableComponent {
+
+}
