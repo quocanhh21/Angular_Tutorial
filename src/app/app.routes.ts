c8a3ca6f6b233532a4ef6b41ce7de324947f4d41
quanh <le.quocanhh21@gmail.com>
2025-03-16 22:28:17 +0700

    Finish user home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/user/home/home.component';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout/user-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes =[
+    {
+        path: '',
+        component: UserLayoutComponent,
+        children: [
+          { path: '', component: HomeComponent }
+        ]
+    },
+    // {
+    //   path: 'admin',
+    //   loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule)
+    // },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Handle unknown routes
+  ];
